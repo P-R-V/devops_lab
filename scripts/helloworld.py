b8b568101b48e6fa8ed66fba6813e2ed02422bb5
Roman Yaronskyi <Roman_Yaronskyi@epam.com>
2019-06-19 22:57:26 +0300

    fix python file v2

--- scripts/helloworld.py
+++ scripts/helloworld.py
@@ -2,6 +2,6 @@ import sys
 print("Hello, World!")
 print("--------------")
 print("Python version")
-print (sys.version)
+print(sys.version)
 print("Version info.")
-print (sys.version_info)
+print(sys.version_info)
